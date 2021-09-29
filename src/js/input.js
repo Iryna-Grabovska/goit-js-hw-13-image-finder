dd17b0903a23466867678fff9c52fd9ad1dfb0c6
Iryna-Grabovska <mrsirynagrabovska@gmail.com>
2021-09-29 22:52:11 +0200

    trying to make lightbox

--- src/js/input.js
+++ src/js/input.js
@@ -3,46 +3,42 @@ import * as PNotifyMobile from "@pnotify/mobile/dist/PNotifyMobile.js";
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 defaultModules.set(PNotifyMobile, {});
- //const element = document.getElementById('.my-element-selector');
-// element.scrollIntoView({
-  //   behavior: 'smooth',
-  //   block: 'end',
-  // });
-  
-  import photoCardTpl from '../templates/photoCard.hbs';
-  import NewApiService from "./apiService";
-  import getRefs from './refs';
-  const refs = getRefs();
+import photoCardTpl from '../templates/photoCard.hbs';
+import * as basicLightbox from 'basiclightbox'
+import NewApiService from "./apiService";
+import getRefs from './refs';
+const refs = getRefs();
 const newApiService = new NewApiService;
-console.log(refs.galleryList);
+const instance = basicLightbox.create(
+  document.querySelector('.js-gallery')
+  )
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
-         refs.loadMoreBtn.style.display= "none";
+refs.loadMoreBtn.style.display= "none";
 
 function onSearch(e) {
   e.preventDefault();
-
-  newApiService.searchQuery =  e.currentTarget.elements.query.value;
+   newApiService.searchQuery =  e.currentTarget.elements.query.value;
   newApiService.resetPage();
+  instance.show();
   newApiService.fetchApiServise().then(photoCard => {
-console.log(photoCard.length)
     clearGalleryList();
-    
     photoCardsMarkup(photoCard);
-     if(!photoCard.length )  {
-            return  error({ text: 'Error! Please enter a more  query!' });
+
+    if(!photoCard.length )  {
+      return  error({ text: 'Error! Please enter a more  query!' });
     }
     if (photoCard.length > 1 && photoCard.length <= 11) {
-
-     return success({ text: 'Hooray! We found images for you!' });
-     }
+      instance.show();
+      
+      return success({ text: 'Hooray! We found images for you!' });
+    }
     if (photoCard.length >= 12 )
     {
       refs.loadMoreBtn.style.display= "";
-     return success({ text: 'Hooray! We found images for you!' });
-
+      return success({ text: 'Hooray! We found images for you!' }); 
     }
-  }
+   }
   )
 }
 function onLoadMore() {
@@ -56,3 +52,9 @@ function photoCardsMarkup(photoCard) {
 function clearGalleryList() {
   refs.galleryList.innerHTML = '';
 }
+
+//const element = document.getElementById('.my-element-selector');
+// element.scrollIntoView({
+ //   behavior: 'smooth',
+ //   block: 'end',
+ // });
